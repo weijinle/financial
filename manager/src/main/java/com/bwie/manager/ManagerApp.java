358fa4aeb94968ad826cea8441af7a45ff159bc3
weijinle <weijinle1995@gmail.com>
2019-09-10 10:14:22 +0800

    financial_魏晋乐第一次提交9/10

--- manager/src/main/java/com/bwie/manager/ManagerApp.java
+++ manager/src/main/java/com/bwie/manager/ManagerApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.bwie.manager"})
+@EntityScan(basePackages = {"com.bwie.entity"})
 public class ManagerApp {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApp.class);
---
+++ manager/src/main/java/com/bwie/manager/controller/ProductController.java
@@ -0,0 +1,71 @@
+package com.bwie.manager.controller;
+
+import com.bwie.entity.Product;
+import com.bwie.manager.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@Slf4j
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Product addProduct(@RequestBody Product product) {
+        log.info("创建产品，参数：{}", product);
+        Product result = productService.addProduct(product);
+        log.info("创建产品，结果：{}", result);
+        return result;
+    }
+
+    /**
+     * 查询单个产品
+     *
+     * @param id 产品编号
+     * @return 返回对应产品或Null
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Product findOne(@PathVariable String id) {
+        log.info("查询单个产品，id={}", id);
+        Product result = productService.findOne(id);
+        log.info("查询单个产品，结果：{}", result);
+        return result;
+    }
+
+    /**
+     * 分页查询产品
+     *
+     * @return
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Page<Product> query(String ids, BigDecimal minRewardRate, BigDecimal maxRewardRate,
+                               String status, @RequestParam(defaultValue = "0") int pageNum,
+                               @RequestParam(defaultValue = "10") int pageSize) {
+        log.info("查询产品，id={}，minRewardRate={}，maxRewardRate={},status={},pageNum={},pageSize={}",
+                ids, minRewardRate, maxRewardRate, status, pageNum, pageSize);
+
+        List<String> idList = null, statusList = null;
+        if (!StringUtils.isEmpty(ids)) {
+            idList = Arrays.asList(ids.split(","));
+        }
+        if (!StringUtils.isEmpty(status)) {
+            statusList = Arrays.asList(status.split(","));
+        }
+        Pageable pageable = new PageRequest(pageNum, pageSize);
+        Page<Product> page = productService.query(idList, minRewardRate, maxRewardRate, statusList, pageable);
+        log.info("查询产品，结果={}", page);
+        return page;
+    }
+}
